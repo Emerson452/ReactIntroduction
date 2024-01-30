8639e41727ad13746d3b47f9e542cdf4506a1e56
emerson452 <emerson@popoyoko.fr>
2024-01-30 15:26:30 +0100

    improve router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import Home from "./pages/Home"
+import Panier from "./pages/Panier"
+import User from "./pages/User"
+
 import './App.css';
 
 function App() {
@@ -9,8 +13,15 @@ function App() {
       <BrowserRouter>
         <header>
           <Link to="/">Home</Link>
+          <Link to="/Panier">Panier</Link>
+          <Link to="/User">User</Link>
         </header>
 
+        <Routes>
+          <Route path='/' Component={Home}></Route>
+          <Route path='/Panier' Component={Panier}></Route>
+          <Route path='/User' Component={User}></Route>
+        </Routes>
 
       </BrowserRouter>
     </div>
